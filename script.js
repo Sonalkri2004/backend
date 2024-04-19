a1ea91e9a5dc9758805cbdf84c0f22ed5a32c54f
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-04-19 12:45:43 +0530

    changes that i make during learn....

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // var a=12;
 // console.log(a);
 
-var figlet = require("figlet");
+// var figlet = require("figlet");
 
-figlet("nothing", function (err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
-  });
+// figlet("nothing", function (err, data) {
+//     if (err) {
+//       console.log("Something went wrong...");
+//       console.dir(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+
+// routing
+
+const express =require("express");
+const app = express();
+app.get("/",function(req,res){
+  res.send("hellllll hlo hlo hlo");
+});
+
+app.get("/profile",function(req,res){
+  res.send("helo this is my profile page");
+});
+
+app.listen(3000);
